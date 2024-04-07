4b7c33cc0cfbed2d8a5b7d1ec369699482e66ff4
ilyaaxenov <aksenov.i@mindbox.cloud>
2024-04-07 22:35:05 +0300

    new morning messages

--- MakimaBot/Model/Events/MorningMessageEvent.cs
+++ MakimaBot/Model/Events/MorningMessageEvent.cs
@@ -9,7 +9,7 @@ public class MorningMessageEvent : IChatEvent
         DateTime.Parse("2023-01-01 05:00:00", CultureInfo.InvariantCulture).TimeOfDay;
     
     private readonly TimeSpan _morningTimeEndUtc =
-        DateTime.Parse("2023-01-01 05:25:00", CultureInfo.InvariantCulture).TimeOfDay;
+        DateTime.Parse("2023-01-01 07:25:00", CultureInfo.InvariantCulture).TimeOfDay;
     
     public bool ShouldLaunch(ChatState chat)
     {
@@ -18,15 +18,47 @@ public class MorningMessageEvent : IChatEvent
         return chat.EventsState.MorningMessage.IsEnabled
                && currentDateTimeUtc.Date != chat.EventsState.MorningMessage.LastTimeStampUtc.Date
                && currentDateTimeUtc.TimeOfDay > _morningTimeStartUtc
-               && currentDateTimeUtc.TimeOfDay < _morningTimeEndUtc;
+               && currentDateTimeUtc.TimeOfDay < _morningTimeEndUtc
+               && currentDateTimeUtc.TimeOfDay > chat.EventsState.MorningMessage.NextStartTimeStampUtc.TimeOfDay;
     }
 
     public async Task HandleEventAsync(TelegramBotClient telegramBotClient, ChatState chat)
     {
         await telegramBotClient.SendTextMessageAsync(
             chatId: chat.ChatId,
-            text: "Доброе утро ❤️");
+            text: GetRandomMessage());
 
         chat.EventsState.MorningMessage.LastTimeStampUtc = DateTime.UtcNow;
+        chat.EventsState.MorningMessage.NextStartTimeStampUtc = GetNextStartTimeStampUtc();
     }
+    
+    public DateTime GetNextStartTimeStampUtc()
+    {
+        var random = new Random();
+        var startTicks = _morningTimeStartUtc.Ticks;
+        var endTicks = _morningTimeEndUtc.Ticks - TimeSpan.FromMinutes(30).Ticks;
+        var randomTicks = startTicks + (long)((endTicks - startTicks) * random.NextDouble());
+        var timeOfDay = TimeSpan.FromTicks(randomTicks);
+
+        return DateTime.UtcNow.Date.AddDays(1).Add(timeOfDay);
+    }
+
+    private string GetRandomMessage()
+    {
+        var random = new Random();
+
+        return _morningMessages[random.Next(0, _morningMessages.Length)];
+    }
+
+    private readonly string[] _morningMessages = {
+        "Доброе утро ❤️",
+        "Доброе утро, красивые 🕊",
+        "Утречко! 🐰",
+        "GOOD MORNING, VIETNAAAAAAM!",
+        "Good morning, sunshines :3",
+        "Утро... спать хочется",
+        "Хорошего дня, я дальше спать 😴",
+        "Чудесного дня, друзья 🤤",
+        "Доброе утро, друзья!"
+    };
 }
--- MakimaBot/Model/State/MorningMessageEventState.cs
+++ MakimaBot/Model/State/MorningMessageEventState.cs
@@ -9,4 +9,7 @@ public class MorningMessageEventState
     
     [JsonPropertyName("lastTimeStampUtc")]
     public required DateTime LastTimeStampUtc { get; set; }
+    
+    [JsonPropertyName("nextStartTimeStampUtc")]
+    public required DateTime NextStartTimeStampUtc { get; set; }
 }
