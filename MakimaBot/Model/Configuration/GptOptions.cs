6849c9fcaa210d72abcb77cdf253eebf97c0098d
ilyaaxenov <ilyaaxenov1999@gmail.com>
2024-08-31 16:24:05 +0300

    use options instead of native config deserialization

---
+++ MakimaBot/Model/Configuration/GptOptions.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MakimaBot.Model.Config;
+
+public class GptOptions
+{
+    public static readonly string SectionName = "gptConfig";
+
+    [Required]
+    public string Url { get; set; }
+
+    [Required]
+    public string ApiKey { get; set; }
+
+    [Required]
+    public string ModelUrl { get; set; }
+}
